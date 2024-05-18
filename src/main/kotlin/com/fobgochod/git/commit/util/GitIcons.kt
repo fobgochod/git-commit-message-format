2c6e6690663c6bf51071a4ca67ec51b020fb445a
fobgochod <fobgochod@gmail.com>
2024-05-19 02:21:12 +0800

    fix: NoSuchClassError in 2024.2

--- src/main/kotlin/com/fobgochod/git/commit/util/GitIcons.kt
+++ src/main/kotlin/com/fobgochod/git/commit/util/GitIcons.kt
@@ -1,7 +1,6 @@
 package com.fobgochod.git.commit.util
 
 import com.intellij.openapi.util.IconLoader
-import icons.CollaborationToolsIcons
 import javax.swing.Icon
 
 object GitIcons {
@@ -12,7 +11,7 @@ object GitIcons {
     fun getNumberIcon(number: Int): Icon {
         return when (number) {
             in 0..9 -> load("/icons/number/$number.svg")
-            else -> CollaborationToolsIcons.Review.CommentReadMany
+            else -> load("/icons/number/9+.svg")
         }
     }
 
